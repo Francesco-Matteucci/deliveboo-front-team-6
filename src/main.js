b06432ae7762052b1042a6df9e6e56d42bbdf9fd
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-12-09 15:56:48 +0100

    imported style.css

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "../src/style.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 const app = createApp(App);
 
 app.use(router);
-app.mount('#app');
+app.mount("#app");
