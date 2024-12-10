11e24244c31eb2a1059f9c8e1629c019f30e28c6
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-12-10 10:24:08 +0100

    .

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -1,67 +1,67 @@
 <script>
-import axios from "axios";
-import 'bootstrap-icons/font/bootstrap-icons.css';
-export default {
-    data() {
-        return {
-            restaurants: [],
-            filteredRestaurants: [],
-            dishes: [],
-            filteredDishes: [],
-            loading: true,
-            error: null,
-        };
-    },
-    methods: {
-        fetchDishes() {
-            const restaurantId = this.$route.params.id;
-            axios
-                .get("http://127.0.0.1:8000/api/dishes")
-                .then((response) => {
-                    console.log("Risposta API piatti:", response.data);
-                    this.dishes = response.data.results;
-                    this.filteredDishes = this.dishes.filter((dish) => dish.restaurant_id === parseInt(restaurantId)
-                    );
-                })
-                .catch((error) => {
-                    console.error("Errore API piatti:", error);
-                    this.error = "Errore nel caricamento dei dati.";
-                })
-                .finally(() => {
-                    this.loading = false;
-                });
+    import axios from "axios";
+    import 'bootstrap-icons/font/bootstrap-icons.css';
+    export default {
+        data() {
+            return {
+                restaurants: [],
+                filteredRestaurants: [],
+                dishes: [],
+                filteredDishes: [],
+                loading: true,
+                error: null,
+            };
         },
-        fetchRestaurants() {
-            const restaurantId = this.$route.params.id;
-            axios
-                .get("http://127.0.0.1:8000/api/restaurants")
-                .then((response) => {
-                    console.log("Risposta API Ristoranti:", response.data);
-                    this.restaurants = response.data.results;
-                    this.filteredRestaurants = this.restaurants.filter(
-                        (restaurant) => restaurant.id === parseInt(restaurantId)
-                    );
-                })
-                .catch((error) => {
-                    console.error("Errore API Ristoranti:", error);
-                    this.error = "Errore nel caricamento dei dati.";
-                })
-                .finally(() => {
-                    this.loading = false;
-                });
-        }
-    },
-    mounted() {
-        this.fetchDishes();
-        this.fetchRestaurants();
-    },
-};
+        methods: {
+            fetchDishes() {
+                const restaurantId = this.$route.params.id;
+                axios
+                    .get("http://127.0.0.1:8000/api/dishes")
+                    .then((response) => {
+                        console.log("Risposta API piatti:", response.data);
+                        this.dishes = response.data.results;
+                        this.filteredDishes = this.dishes.filter((dish) => dish.restaurant_id === parseInt(restaurantId)
+                        );
+                    })
+                    .catch((error) => {
+                        console.error("Errore API piatti:", error);
+                        this.error = "Errore nel caricamento dei dati.";
+                    })
+                    .finally(() => {
+                        this.loading = false;
+                    });
+            },
+            fetchRestaurants() {
+                const restaurantId = this.$route.params.id;
+                axios
+                    .get("http://127.0.0.1:8000/api/restaurants")
+                    .then((response) => {
+                        console.log("Risposta API Ristoranti:", response.data);
+                        this.restaurants = response.data.results;
+                        this.filteredRestaurants = this.restaurants.filter(
+                            (restaurant) => restaurant.id === parseInt(restaurantId)
+                        );
+                    })
+                    .catch((error) => {
+                        console.error("Errore API Ristoranti:", error);
+                        this.error = "Errore nel caricamento dei dati.";
+                    })
+                    .finally(() => {
+                        this.loading = false;
+                    });
+            }
+        },
+        mounted() {
+            this.fetchDishes();
+            this.fetchRestaurants();
+        },
+    };
 </script>
 
 <template>
-    <header class="d-flex flex-column flex-lg-row justify-content-around mt-4">
+    <header class="d-flex flex-column flex-lg-row justify-content-around mt-4 container-md-4">
         <!-- Banner Header -->
-        <div class="col-12 col-lg-6 row p-5 align-items-center rounded-5 mx-0 bg-header">
+        <div class="col-12 col-lg-6 row p-5 align-items-center rounded-5 mx-0 my-2 bg-header">
             <div class="col-md-6 text-center">
                 <img src="../assets/logo-deliveboo.webp" class="img-fluid rounded-circle" alt="">
             </div>
@@ -148,170 +148,170 @@ export default {
 
 
 <style scoped>
-/*Restaurants*/
+    /*Restaurants*/
 
-.bg-header {
-    background: rgb(238, 174, 202);
-    background: radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(148, 187, 233, 1) 100%);
-}
+    .bg-header {
+        background: rgb(238, 174, 202);
+        background: radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(148, 187, 233, 1) 100%);
+    }
 
-.restaurant-card {
-    border: 1px solid #ddd;
-    border-radius: 10px;
-    overflow: hidden;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-    transition: transform 0.3s ease, box-shadow 0.3s ease;
-}
+    .restaurant-card {
+        border: 1px solid #ddd;
+        border-radius: 10px;
+        overflow: hidden;
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+        transition: transform 0.3s ease, box-shadow 0.3s ease;
+    }
 
-.restaurant-card:hover {
-    box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
-}
+    .restaurant-card:hover {
+        box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
+    }
 
-.restaurant-banner {
-    height: 200px;
-    background-size: cover;
-    background-position: center;
-    position: relative;
-}
+    .restaurant-banner {
+        height: 200px;
+        background-size: cover;
+        background-position: center;
+        position: relative;
+    }
 
-.restaurant-overlay {
-    background: rgba(0, 0, 0, 0.5);
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
+    .restaurant-overlay {
+        background: rgba(0, 0, 0, 0.5);
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
-.restaurant-details {
-    background: #fff;
-    padding: 20px;
-    border-top: 2px solid #ddd;
-}
+    .restaurant-details {
+        background: #fff;
+        padding: 20px;
+        border-top: 2px solid #ddd;
+    }
 
-.restaurant-details h2 {
-    font-size: 1.5rem;
-}
+    .restaurant-details h2 {
+        font-size: 1.5rem;
+    }
 
-.restaurant-details p {
-    font-size: 1rem;
-    line-height: 1.5;
-}
+    .restaurant-details p {
+        font-size: 1rem;
+        line-height: 1.5;
+    }
 
-.restaurant-description {
-    font-size: 0.9rem;
-    line-height: 1.5;
-    color: #6c757d;
-}
+    .restaurant-description {
+        font-size: 0.9rem;
+        line-height: 1.5;
+        color: #6c757d;
+    }
 
-/*Dishes*/
+    /*Dishes*/
 
-.dish-list {
-    background-color: rgba(240, 240, 240, 0.404);
-}
+    .dish-list {
+        background-color: rgba(240, 240, 240, 0.404);
+    }
 
-.dish-card {
-    position: relative;
-    border: none;
-    border-radius: 10px;
-    overflow: hidden;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-    transition: transform 0.3s ease, box-shadow 0.3s ease;
-}
+    .dish-card {
+        position: relative;
+        border: none;
+        border-radius: 10px;
+        overflow: hidden;
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+        transition: transform 0.3s ease, box-shadow 0.3s ease;
+    }
 
-.dish-card:hover {
+    .dish-card:hover {
 
-    box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.5);
-}
+        box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.5);
+    }
 
-.dish-card-image {
-    height: 200px;
-    background-size: cover;
-    background-position: center;
-    position: relative;
-    transition: opacity 0.3s ease;
-}
+    .dish-card-image {
+        height: 200px;
+        background-size: cover;
+        background-position: center;
+        position: relative;
+        transition: opacity 0.3s ease;
+    }
 
-.dish-card-back {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.8);
-    padding: 15px;
-    opacity: 0;
-    transition: opacity 0.3s ease;
-    border-radius: 10px;
-}
+    .dish-card-back {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0, 0, 0, 0.8);
+        padding: 15px;
+        opacity: 0;
+        transition: opacity 0.3s ease;
+        border-radius: 10px;
+    }
 
-.not-available {
-    color: #e2e2e2;
-    cursor: not-allowed;
-    pointer-events: none;
-    opacity: 1;
-}
+    .not-available {
+        color: #e2e2e2;
+        cursor: not-allowed;
+        pointer-events: none;
+        opacity: 1;
+    }
 
-.dish-card-overlay {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    background: rgba(0, 0, 0, 0.4);
-    color: #fff;
-    padding: 10px;
-    text-align: center;
-}
+    .dish-card-overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        background: rgba(0, 0, 0, 0.4);
+        color: #fff;
+        padding: 10px;
+        text-align: center;
+    }
 
-.add-to-cart-button {
-    background-color: rgb(197, 148, 57);
-    border: rgb(197, 148, 57);
-}
+    .add-to-cart-button {
+        background-color: rgb(197, 148, 57);
+        border: rgb(197, 148, 57);
+    }
 
-.add-to-cart-button:hover {
-    scale: 1.1;
-}
+    .add-to-cart-button:hover {
+        scale: 1.1;
+    }
 
-.dish-view-description {
-    padding: 10px;
-    background-color: rgb(197, 148, 57);
-}
+    .dish-view-description {
+        padding: 10px;
+        background-color: rgb(197, 148, 57);
+    }
 
-.dish-card:hover .dish-card-image {
-    opacity: 0;
-}
+    .dish-card:hover .dish-card-image {
+        opacity: 0;
+    }
 
-.dish-card:hover .dish-card-back {
-    opacity: 1;
-}
+    .dish-card:hover .dish-card-back {
+        opacity: 1;
+    }
 
-.dish-description {
-    max-height: 100px;
-    overflow-y: auto;
-    padding: 10px;
-    border-radius: 5px;
-    color: #fff;
-    text-align: left;
-}
+    .dish-description {
+        max-height: 100px;
+        overflow-y: auto;
+        padding: 10px;
+        border-radius: 5px;
+        color: #fff;
+        text-align: left;
+    }
 
-.dish-description::-webkit-scrollbar {
-    width: 8px;
-}
+    .dish-description::-webkit-scrollbar {
+        width: 8px;
+    }
 
-.dish-description::-webkit-scrollbar-thumb {
-    background-color: #c59439;
-    border-radius: 10px;
-}
+    .dish-description::-webkit-scrollbar-thumb {
+        background-color: #c59439;
+        border-radius: 10px;
+    }
 
-.dish-description::-webkit-scrollbar-thumb:hover {
-    background-color: #e3b15e;
-}
+    .dish-description::-webkit-scrollbar-thumb:hover {
+        background-color: #e3b15e;
+    }
 
-.dish-description::-webkit-scrollbar-track {
-    background: #2e2e2e;
-    border-radius: 10px;
-}
+    .dish-description::-webkit-scrollbar-track {
+        background: #2e2e2e;
+        border-radius: 10px;
+    }
 </style>
